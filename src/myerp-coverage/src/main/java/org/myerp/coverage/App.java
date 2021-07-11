99c2ad0360e8ae1aef77a446d074838c7e707534
STEIGERWALD <47118413+Steigerwald@users.noreply.github.com>
2021-07-11 22:54:34 +0200

    version avec agregate de jacoco et correction bug avec verify

---
+++ src/myerp-coverage/src/main/java/org/myerp/coverage/App.java
@@ -0,0 +1,13 @@
+package org.myerp.coverage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
